ec93976c5c60c7b48772075fcb0afa693d797982
sauceky <sodiqadigun@gmail.com>
2024-10-26 00:46:59 +0100

    added sout

---
+++ src/ForCounter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class ForCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        // for statement header
+        // counter 1;
+        // counter++ post increment
+        // ++counter pre increment
+
+// keyword for (instalise count variable, increment)
+        for (int counter = 1; counter <= 10; counter++) {
+
+            System.out.printf("%d ", counter);
+            //For reputation While
+        }
+    }
+}
---
+++ src/WhileCounter.java
@@ -0,0 +1,13 @@
+public class WhileCounter {
+    public static void main(String[] args) {
+        int counter = 1;
+
+        while (counter <= 10) { // loop continuation condition
+
+            System.out.print(counter);
+
+            counter++; // increase control variable
+            //While reputation
+        }
+    }
+}
